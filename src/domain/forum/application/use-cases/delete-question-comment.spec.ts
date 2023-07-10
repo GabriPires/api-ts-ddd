b8b7b9fec3a8a6336c1c1aeb8068f6accd417d29
Gabriel Pires <gabpiresdev@gmail.com>
2023-07-10 19:22:21 -0300

    feat: delete question comment use case

---
+++ src/domain/forum/application/use-cases/delete-question-comment.spec.ts
@@ -0,0 +1,43 @@
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { makeQuestionComment } from 'test/factories/make-question-comment'
+import { InMemoryQuestionCommentsRepository } from 'test/repositories/in-memory-question-comments-repository'
+import { DeleteQuestionCommentUseCase } from './delete-question-comment'
+
+let inMemoryQuestionCommentsRepository: InMemoryQuestionCommentsRepository
+let sut: DeleteQuestionCommentUseCase
+
+describe('Delete Question Comment', () => {
+  beforeEach(() => {
+    inMemoryQuestionCommentsRepository =
+      new InMemoryQuestionCommentsRepository()
+    sut = new DeleteQuestionCommentUseCase(inMemoryQuestionCommentsRepository)
+  })
+
+  test('should be able to delete a comment question', async () => {
+    const newQuestion = makeQuestionComment()
+
+    inMemoryQuestionCommentsRepository.create(newQuestion)
+
+    await sut.execute({
+      questionCommentId: newQuestion.id.toString(),
+      authorId: newQuestion.authorId.toString(),
+    })
+
+    expect(inMemoryQuestionCommentsRepository.items).toHaveLength(0)
+  })
+
+  test('should not be able to delete a question comment from another user', async () => {
+    const newQuestion = makeQuestionComment({
+      authorId: new UniqueEntityId('author-1'),
+    })
+
+    inMemoryQuestionCommentsRepository.create(newQuestion)
+
+    expect(() =>
+      sut.execute({
+        questionCommentId: newQuestion.id.toString(),
+        authorId: 'author-2',
+      }),
+    ).rejects.toBeInstanceOf(Error)
+  })
+})
--- test/repositories/in-memory-question-comments-repository.ts
+++ test/repositories/in-memory-question-comments-repository.ts
@@ -6,7 +6,23 @@ export class InMemoryQuestionCommentsRepository
 {
   public items: QuestionComment[] = []
 
+  async findById(id: string): Promise<QuestionComment | null> {
+    const questionComment = this.items.find((item) => item.id.toString() === id)
+
+    if (!questionComment) return null
+
+    return questionComment
+  }
+
   async create(questionComment: QuestionComment) {
     this.items.push(questionComment)
+  }
+
+  async delete(questionComment: QuestionComment): Promise<void> {
+    const index = this.items.findIndex(
+      (item) => item.id.toString() === questionComment.id.toString(),
+    )
+
+    this.items.splice(index, 1)
   }
 }
