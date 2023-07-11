138fc9dc87f57c8aaa1a70662ec6a438633ecb30
Gabriel Pires <gabpiresdev@gmail.com>
2023-07-11 19:55:44 -0300

    refactor: using either to return success or error to use cases

--- src/domain/forum/application/use-cases/fetch-question-answers.ts
+++ src/domain/forum/application/use-cases/fetch-question-answers.ts
@@ -1,3 +1,4 @@
+import { Either, right } from '@/core/either'
 import { Answer } from '../../enterprise/entities/answer'
 import { AnswersRepository } from '../repositories/answers-repository'
 
@@ -6,9 +7,12 @@ interface FetchQuestionAnswersRequest {
   questionId: string
 }
 
-interface FetchQuestionAnswersResponse {
-  answer: Answer[]
-}
+type FetchQuestionAnswersResponse = Either<
+  null,
+  {
+    answer: Answer[]
+  }
+>
 
 export class FetchQuestionAnswers {
   constructor(private answersRepository: AnswersRepository) {}
@@ -24,6 +28,6 @@ export class FetchQuestionAnswers {
       },
     )
 
-    return { answer }
+    return right({ answer })
   }
 }
