46b438e2c2a56ef8e8289e0fa8214133d3ec7026
Gabriel Pires <gabpiresdev@gmail.com>
2023-07-25 17:22:29 -0300

    feat: domain event to send notification when chose a question best answer

---
+++ src/domain/notification/application/subscribers/on-question-best-answer-chosen.spec.ts
@@ -0,0 +1,77 @@
+import { makeAnswer } from 'test/factories/make-answer'
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
+import { InMemoryAnswerAttachmentsRepository } from 'test/repositories/in-memory-answer-attachments-repository'
+import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { QuestionAttachmentsRepository } from '@/domain/forum/application/repositories/question-attachments-repository'
+import { InMemoryQuestionAttachmentsRepository } from 'test/repositories/in-memory-question-attachments-repository'
+import {
+  SendNotificationUseCase,
+  SendNotificationUseCaseRequest,
+  SendNotificationUseCaseResponse,
+} from '../use-cases/send-notification'
+import { InMemoryNotificationsRepository } from 'test/repositories/in-memory-notifications-repository'
+import { NotificationsRepository } from '../repositories/notifications-repository'
+import { makeQuestion } from 'test/factories/make-question'
+import { SpyInstance } from 'vitest'
+import { waitFor } from 'test/utils/wait-for'
+import { OnQuestionBestAnswerChosen } from './on-question-best-answer-chosen'
+
+let inMemoryNotificationsRepository: NotificationsRepository
+let inMemoryQuestionAttachmentsRepository: QuestionAttachmentsRepository
+let inMemoryQuestionsRepository: QuestionsRepository
+let inMemoryAnswersRepository: InMemoryAnswersRepository
+let inMemoryAnswerAttachmentsRepository: InMemoryAnswerAttachmentsRepository
+let sendNotificationUseCase: SendNotificationUseCase
+
+let sendNotificationExecuteSpy: SpyInstance<
+  [SendNotificationUseCaseRequest],
+  Promise<SendNotificationUseCaseResponse>
+>
+
+describe('On Question Best Answer Chosen', () => {
+  beforeEach(() => {
+    inMemoryQuestionAttachmentsRepository =
+      new InMemoryQuestionAttachmentsRepository()
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository(
+      inMemoryQuestionAttachmentsRepository,
+    )
+    inMemoryAnswerAttachmentsRepository =
+      new InMemoryAnswerAttachmentsRepository()
+    inMemoryNotificationsRepository = new InMemoryNotificationsRepository()
+    inMemoryAnswersRepository = new InMemoryAnswersRepository(
+      inMemoryAnswerAttachmentsRepository,
+    )
+    sendNotificationUseCase = new SendNotificationUseCase(
+      inMemoryNotificationsRepository,
+    )
+
+    sendNotificationExecuteSpy = vi.spyOn(sendNotificationUseCase, 'execute')
+
+    // eslint-disable-next-line no-new
+    new OnQuestionBestAnswerChosen(
+      inMemoryAnswersRepository,
+      sendNotificationUseCase,
+    )
+  })
+
+  it('should send a notification when an question has new best answer chosen', async () => {
+    const question = makeQuestion()
+    const answer = makeAnswer({
+      questionId: question.id,
+    })
+
+    inMemoryQuestionsRepository.create(question)
+    inMemoryAnswersRepository.create(answer)
+
+    // create event
+    question.bestAnswerId = answer.id
+
+    // dispatch event
+    inMemoryQuestionsRepository.save(question)
+
+    await waitFor(() => {
+      expect(sendNotificationExecuteSpy).toHaveBeenCalled()
+    })
+  })
+})
